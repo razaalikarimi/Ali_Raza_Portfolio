d1d91d710b5063b988cc946d384cfc92b1795a56
razaalikarimi <aliraza85211@gmail.com>
2025-05-07 03:06:33 +0530

    commit

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -3,7 +3,7 @@ const AboutMeImage = () => {
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src="public/images/HexaPic.png"
+          src="https://drive.google.com/file/d/1Li_uAIavNpE1Gne2mSLsbJewMVukPTK1/view?usp=sharing"
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
