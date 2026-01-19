616403726412cc04ba0e2b72bbd7f3b7f69c38cf
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2026-01-19 20:54:10 +0530

    Update ExperienceTopLeft.jsx

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -9,7 +9,7 @@ const ExperienceTopLeft = () => {
       <div className="flex justify-center items-center gap-4">
         <ExperienceInfo number="1" text="Years" />
         <p className="font-bold text-6xl text-lightBrown">-</p>
-        <ExperienceInfo number="3-4" text="Websites" />
+        <ExperienceInfo number="6-7" text="Websites" />
       </div>
       <p className="text-center">
         With 1+ years of experience building dynamic and user-friendly web
